b908c933202ff80dfd68b7ba4eb6f7554be1527d
Dakota Blackwell <dakota.blackwell@student.ufv.ca>
2021-01-09 22:45:08 -0800

    Added more examples.

--- js/imports.js
+++ js/imports.js
@@ -6,7 +6,7 @@ function place(name, location, extras) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             element.innerHTML = this.responseText;
-            extras()
+            extras();
         }
     }
     xhttp.open("GET", location, true);
@@ -18,14 +18,28 @@ function placeNavBar() {
         active = document.getElementById(document.title.replaceAll(" ", ""));
         active.classList.add("active");
     }
-    place("navbar", "/fragments/navbar.html", extras)
+    place("navbar", "/fragments/navbar.html", extras);
+}
+
+function fixEmail() {
+    // I purposefully put a copyright symbol in the email address to fool low-quality
+    // scrapers (which don't run JavaScript). This code removes it once loaded.
+    span = document.getElementById("emailSpan");
+    email = span.innerHTML.replaceAll("©", "@");
+    span.innerHTML = '<a href="mailto:' + email + '">' + email + '</a>';
 }
 
 function jsCheck() {
     document.getElementById("jsCheck").remove();
+    document.getElementById("main").style.display = "block";
 }
 
 function onLoad() {
     placeNavBar();
     jsCheck();
+}
+
+function onLoadContact() {
+    onLoad()
+    fixEmail()
 }
